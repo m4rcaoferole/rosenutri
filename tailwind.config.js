d8dfd63ebc2608743930e4962d1f885a9b0f217b
Marcos Ferole <marcao.vfo@gmail.com>
2024-02-16 06:49:14 +0900

    config colors of projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    colors: {
+      midori100: '#155b41',
+      midori200: '#18433c',
+      midori300: '#0f4730',
+      murasaki100: '#821d78',
+      murasaki200: '#68195d',
+      murasaki300: '#631b52',
+      shiro100: '#f2f0e4',
+      shiro200: '#fff6d9',
+      shiro300: '#f9ebb4'
+    }
   },
   plugins: [],
 }
