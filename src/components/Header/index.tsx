ae804230ea892d2ea5aca311966093e729564940
Marcos Vinicius <marcao.vfo@gmail.com>
2025-05-01 23:09:41 +0900

    fix: correct size logo and space header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
+import { List, X } from '@phosphor-icons/react';
 import Image from 'next/image';
 import { useState } from 'react';
-import logoCircle from '../../assets/logo-rose-circular-original.png';
 import logoRose from '../../assets/logo-rose-original.png';
 
 const NAV_LINKS = [
-  { href: '#services', label: 'Serviços' },
+  { href: '#services', label: 'Serviços Nutricionais' },
   { href: '#about', label: 'Sobre a Nutri' },
   { href: '#consult', label: 'Consulta Online' },
   {
@@ -21,28 +21,28 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-white top-0 w-full shadow-md z-50 relative">
+    <header className="bg-white w-full shadow z-50 relative">
       {/* <div className="container max-w-none flex items-center px-4 md:justify-between md:shadow-lg md:shadow-murasaki100/20"> */}
-      <div className="max-w-6xl mx-auto flex justify-between items-center ">
+      <div className="max-w-6xl px-8 py-2 mx-auto flex justify-between items-center">
         <div className="hidden md:block">
           <Image src={logoRose} alt="Logo da Nutri" width={150} height={150} />
         </div>
 
         <div className="md:hidden px-8 flex items-center w-full relative">
           <div className="md:hidden ">
-            <Image src={logoCircle} alt="Logo da Circuclo Nutri" width={130} height={130} />
+            <Image src={logoRose} alt="Logo da Circuclo Nutri" width={130} height={130} />
           </div>
           <button
             onClick={() => setIsOpen(!isOpen)}
             className="md:hidden absolute right-8 text-3xl text-murasaki200 focus:outline-none"
             aria-label="Abrir menu"
           >
-            {!isOpen ? '☰' : 'x'}
+            {!isOpen ? <List size={28} color="#C087D7" /> : <X size={28} color="#C087D7" />}
           </button>
         </div>
 
         {/* Links de divegação (Desktop) */}
-        <nav className="hidden md:flex space-x-8 font-semibold">
+        <nav className="hidden md:flex gap-4 space-x-6">
           {NAV_LINKS.map((desktop) => (
             <a
               key={desktop.href}
@@ -58,8 +58,8 @@ function Header() {
 
       {/* Menu Mobile */}
       {isOpen && (
-        <div className="bg-white top-full pb-8 shadow-lg">
-          <nav className="flex items-center flex-col gap-3">
+        <div className="absolute w-full bg-white shadow-lg rounded p-6 z-50">
+          <nav className="flex items-center flex-col gap-4">
             {NAV_LINKS.map((mobile) => (
               <a
                 key={mobile.href}
