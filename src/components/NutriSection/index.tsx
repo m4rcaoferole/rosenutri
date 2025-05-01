2e04601571fca7f21c6c20a291d68af98daecb1a
Marcos Vinicius <marcao.vfo@gmail.com>
2025-05-01 23:10:30 +0900

    feat: new style

--- src/components/NutriSection/index.tsx
+++ src/components/NutriSection/index.tsx
@@ -1,36 +1,59 @@
-import React from 'react';
+'use client';
+
+import { WhatsappLogo } from '@phosphor-icons/react';
+import Image from 'next/image';
+
+import logoCircleRose from '../../assets/logo-rose-circular-original.png';
+import nutriPhoto from '../../assets/nutriPhoto.png';
 
 export default function NutriSection() {
   return (
-    <section className="bg-white">
-      {/* Background Decoration */}
+    <section className="bg-gradient-to-r from-white via-white to-murasaki100/50 px-6 pt-8 md:pt-6 sm:pt-12">
+      <div className="max-w-4xl mx-auto flex items-center">
+        {/* Texto e Botão */}
+        <div className="text-center flex flex-col gap-3 md:pr-10 md:text-left space-x-6">
+          <div className="flex items-center gap-6">
+            <Image src={logoCircleRose} alt="Logo da Nutri" width={150} height={150} />
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              Acompanhamento{' '}
+              <span className="text-murasaki300">
+                Nutricional <br />
+              </span>{' '}
+              100% Online e<span className="text-murasaki300"> Personalizado</span>
+            </h1>
+          </div>
 
-      <div className="container mx-auto flex flex-col items-center justify-center py-20 px-5 text-center md:px-10">
-        {/* Título e Subtítulo */}
-        <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl lg:text-6xl">
-        Emagreça com <span className="text-murasaki300">leveza</span> e <span className="text-murasaki300">equilíbrio</span>.
-        </h1>
-        <h2 className="mt-5 text-lg font-bold text-midori300 sm:text-xl">
-          Transforme seu corpo e sua rotina com saúde
-        </h2>
-         <p className="mt-5 text-lg text-gray-700 sm:text-xl">
-          Consultoria nutricional personalizada para uma vida mais saudável e equilibrada.
-        </p>
+          <h2 className="text-sm md:text-right font-semibold text-midori300 sm:text-base">
+            Cada mulher é única e sua alimentação também precisa ser.
+          </h2>
 
-        {/* Call to Action (Botões) */}
-        <div className="mt-8 flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            href="#services"
-            className="rounded-lg bg-green-600 px-6 py-3 text-white font-semibold shadow-md hover:bg-green-700 transition"
-          >
-            Conheça os Serviços
-          </a>
-          <a
-            href="#contact"
-            className="rounded-lg border border-green-600 px-6 py-3 text-green-600 font-semibold shadow-md hover:bg-green-50 transition"
-          >
-            Fale Conosco
-          </a>
+          <p className="text-gray-700 text-lg sm:text-base">
+            No meu atendimento online, você recebe um plano alimentar adaptado à sua rotina,
+            objetivos e gostos, sem restrições desnecessárias e com foco em resultados reais.
+          </p>
+
+          <div className="flex justify-center item-center w-30 mb-10">
+            <a
+              href="https://wa.me/819038354891"
+              target="_blank"
+              // className="bg-murasaki300 text-white font-bold px-8 py-4 rounded shadow-lg hover:bg-murasaki100 transition"
+              className="bg-gradient-to-r from-murasaki300 to-murasaki100 text-white font-bold px-8 py-4 rounded shadow-lg hover:opacity-90 transition"
+            >
+              <WhatsappLogo size={28} className="inline mr-2" />
+              Agendar pelo WhatsApp
+            </a>
+          </div>
+        </div>
+
+        {/* Foto da Nutricionista */}
+        <div className="hidden md:block animate-fadeIn">
+          <Image
+            src={nutriPhoto}
+            alt="Foto da Nutricionista Rosemary Azuma"
+            width={500}
+            height={500}
+            className="object-cover"
+          />
         </div>
       </div>
     </section>
